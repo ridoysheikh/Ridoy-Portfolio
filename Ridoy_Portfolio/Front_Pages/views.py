a2b122cfd1b68d3838a5b0631dead23b8e84fb11
Ridoy sheikh <111294017+ridoysheikh@users.noreply.github.com>
2023-02-06 01:33:02 +0600

    added Resume Page

--- Ridoy_Portfolio/Front_Pages/views.py
+++ Ridoy_Portfolio/Front_Pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     title = "Home"
     return render(request, 'front/home.html',context={'title': title})
+
+def resumes(request):
+    title = "Resume"
+    return render(request, 'front/resume.html',context={'title': title})
