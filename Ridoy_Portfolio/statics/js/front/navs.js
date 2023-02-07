8994b87141ff475a53ca4f520de3e5dcafdfef95
Ridoy sheikh <111294017+ridoysheikh@users.noreply.github.com>
2023-02-08 01:23:08 +0600

    adeed Resume And Gitignore

--- Ridoy_Portfolio/statics/js/front/navs.js
+++ Ridoy_Portfolio/statics/js/front/navs.js
@@ -2,12 +2,12 @@ function tooglenavs() {
     if(document.querySelector(".main_nav").style.left == "-300px"){
         document.querySelector(".main_nav").style.left = "0px";
         document.querySelector(".togle_btn").style.left = "300px";
-        document.querySelector(".material-symbols-outlined").style.transform = "rotateY(180deg)";
+        document.querySelector(".arrow").style.transform = "rotateY(180deg)";
 
     } else{
         document.querySelector(".main_nav").style.left = "-300px";
         document.querySelector(".togle_btn").style.left = "0px";
-        document.querySelector(".material-symbols-outlined").style.transform = "rotateY(0deg)";
+        document.querySelector(".arrow").style.transform = "rotateY(0deg)";
 
     };
     
