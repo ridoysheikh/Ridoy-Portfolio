9cedfb3b54c2ecb41dfb89d3efc9a573f7c552dd
Ridoy sheikh <111294017+ridoysheikh@users.noreply.github.com>
2023-04-29 23:45:58 +0600

    added pdf tools

--- Ridoy_Portfolio/tools/views.py
+++ Ridoy_Portfolio/tools/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 import requests
-from django.http import HttpResponse, StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse, FileResponse
 import youtube_dl
-import re
+import re,sys ,os, io
+from io import BytesIO
+from pdf2image import convert_from_bytes
+import zipfile
+
 
 # Create your views here.
 def you_tube(request):
@@ -57,6 +61,9 @@ def you_tube(request):
                 
             return render(request, 'tools/youtube.html', {"link": video_url, "video": stream_info_list})
         except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            e=f'{exc_type}, {fname}, {exc_tb.tb_lineno}'
             return render(request,'tools/youtube.html',{"err":str(e)})
     if request.GET.get('url'):
         try:
@@ -91,14 +98,13 @@ def you_tube(request):
             ydl = youtube_dl.YoutubeDL(ydl_opts)
             info_dict = ydl.extract_info(video_url, download=False)
 
-
             # Get Different Sites Vidoes Informations
             if re.match(r"https?://www\.facebook\.com/", video_url):
                 video_url = info_dict['entries'][0]['formats']
                 for vdo in video_url:
-                    if vdo['format_id'] == itag:
+                    if vdo['format'] == itag:
                         video_url = vdo['url']
-                    fname=f"fb-video{vdo['format_id']}"
+                    fname=f"fb-video{vdo['format']}"
             else:
                 video_url = info_dict['formats']
                 for vdo in video_url:
@@ -120,5 +126,49 @@ def you_tube(request):
             return response
 
         except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            e=f'{exc_type}, {fname}, {exc_tb.tb_lineno}'
             return HttpResponse(f"Error: {e}")
     return render(request,'tools/youtube.html',{})
+def pdf_tool(request):
+    if request.method == "POST":
+        file = request.FILES['file']
+        try:
+            if file:
+                # Read the uploaded PDF file
+                pdf_file = file.read()
+                # Convert the PDF to a list of PNG images
+                images = convert_from_bytes(pdf_file)
+                
+                # Create a temporary file to hold the ZIP archive
+                zip_file = zipfile.ZipFile('temp.zip', 'w')
+                
+                # Add each image to the ZIP archive
+                for i, image in enumerate(images):
+                    # Save the PNG image to an in-memory buffer
+                    buffer = BytesIO()
+                    image.save(buffer, format='PNG')
+                    
+                    # Add the image to the ZIP archive with a unique filename
+                    filename = f'page_{i+1}.png'
+                    zip_file.writestr(filename, buffer.getvalue())
+                
+                # Close the ZIP archive
+                zip_file.close()
+                
+                # Create a response object with the ZIP archive as the content
+                response = HttpResponse(open('temp.zip', 'rb'), content_type='application/zip')
+                response['Content-Disposition'] = 'attachment; filename="output.zip"'
+                
+                # Delete the temporary file
+                os.remove('temp.zip')
+                
+                # Return the response
+                return response
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            e=f'{exc_type}, {fname}, {exc_tb.tb_lineno}'
+            return render(request,'tools/pdf.html',{"err":str(e)})
+    return render(request,'tools/pdf.html',{})
